0c79d221523c07a53963b159131b358b3bbf24be
shaikhhamza11 <shaikh.hamza1199@gmail.com>
2025-03-01 20:13:22 +0530

    Feat: Header added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,20 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter, Calistoga } from 'next/font/google';
+import './globals.css';
+import { twMerge } from 'tailwind-merge';
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: 'Hamza | Portfolio',
+  description: 'Hamza Shaikh Portfolio',
 };
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
+
+const calistoga = Calistoga({
+  subsets: ['latin'],
+  variable: '--font-serif',
+  weight: ['400'],
+});
 
 export default function RootLayout({
   children,
@@ -12,8 +22,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body
+        className={twMerge(
+          inter.className,
+          calistoga.className,
+          'bg-gray-900 text-white antialiased font-sans',
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,23 @@
 export const Header = () => {
-  return <div>Header Section</div>;
+  return (
+    <div className='flex justify-center items-center relative top-3 '>
+      <nav className='flex gap-1 border border-white/10 rounded-full bg-white/10 backdrop-blur-0'>
+        <a href='#' className='nav-item'>
+          Home
+        </a>
+        <a href='#' className='nav-item'>
+          Projects
+        </a>
+        <a href='#' className='nav-item'>
+          About
+        </a>
+        <a
+          href='#'
+          className='nav-item bg-white text-gray-900 hover:text-gray-900 hover:bg-white/70'
+        >
+          Contact
+        </a>
+      </nav>
+    </div>
+  );
 };
